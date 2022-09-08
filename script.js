fd14bb8ce309ebb3aad60dcf72da577f2a074e00
Lahiru Gayantha <lahirugayantha@yaho.com>
2022-09-08 20:35:03 +0530

    Login completed

---
+++ script.js
@@ -0,0 +1,10 @@
+const login = () => {
+    const userName = document.getElementById('userName').value;
+    const password = document.getElementById('password').value;
+    if(userName === "Lahiru")
+    {
+        alert("Success!");
+    }else{
+        alert("Fail!");
+    }
+}
